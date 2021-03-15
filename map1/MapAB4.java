ab3ecb9d20cf60429af1f7b1367cc9e2e82961f5
Daniel Zhang <45671923+danielrzhang@users.noreply.github.com>
2021-03-14 23:27:26 -0400

    Add files via upload

---
+++ map1/MapAB4.java
@@ -0,0 +1,26 @@
+package map1;
+
+import java.util.Map;
+
+public class MapAB4 {
+
+	public static Map<String, String> mapAB4(Map<String, String> map) {
+		if (map.containsKey("a") && map.containsKey("b")) {
+			if (map.get("a").length() > map.get("b").length()) {
+				map.put("c", map.get("a"));
+			} else if (map.get("a").length() < map.get("b").length()) {
+				map.put("c", map.get("b"));
+			} else {
+				map.put("a", "");
+				map.put("b", "");
+			}
+		}
+		return map;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
